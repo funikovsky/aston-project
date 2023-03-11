ee029a5bd852b926de62fe1c05ab388645dca7b1
Alexsey Funikov <it-kadui@mail.ru>
2023-03-11 16:21:53 +0300

    Реализовал применение Контекст API

--- src/components/CustomLink/CustomLink.tsx
+++ src/components/CustomLink/CustomLink.tsx
@@ -1,8 +1,9 @@
-import { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react';
+import { DetailedHTMLProps, HTMLAttributes, ReactNode, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './CustomLink.module.scss';
 
 import cn from 'classnames';
+import { ThemeContext } from '../../hoc/ThemeProvider';
 
 export interface Props extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
   children: ReactNode;
@@ -10,8 +11,18 @@ export interface Props extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>,
 }
 
 export const CustomLink = ({ children, href, className, ...props }: Props) => {
+  const themeContext = useContext(ThemeContext);
   return (
-    <div className={cn(styles.tag, className, href, { ...props })}>
+    <div
+      className={cn(
+        styles.tag,
+        className,
+        href,
+        {
+          [styles.dark]: themeContext?.theme == 'dark',
+        },
+        { ...props },
+      )}>
       <Link to={href}>{children}</Link>
     </div>
   );
--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,10 +1,13 @@
-import { useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 import { Outlet, useMatch } from 'react-router-dom';
+import cn from 'classnames';
 
 import { CustomLink } from '../components/CustomLink/CustomLink';
 import { FlexBlock } from '../components/FlexBlock/FlexBLock';
 import { SearchBlock } from '../components/SearchBlock/SearchBlock';
+import { Switch } from '../components/Switch/Switch';
 import { UserEnterBlock } from '../components/UserEnterBlock/UserEnterBlock';
+import { ThemeContext } from '../hoc/ThemeProvider';
 import { useAppDispatch } from '../hooks/useAppHooks';
 import { useAuth } from '../hooks/useAuth';
 import { getTrendingGifs } from '../redux/slice/asyncThunks/getTrendingGifThunk';
@@ -15,21 +18,27 @@ export const MainLayout = () => {
   const match = useMatch('/');
   const dispatch = useAppDispatch();
   const { email, isAuth } = useAuth();
+  const themeContext = useContext(ThemeContext);
+
   useEffect(() => {
     dispatch(getTrendingGifs());
   }, []);
 
   return (
-    <div className={styles.wrapper}>
+    <div
+      className={cn(styles.wrapper, {
+        [styles.dark]: themeContext?.theme == 'dark',
+      })}>
       <FlexBlock className={styles.header}>
-        <div className={styles.btnblock}>
-          {' '}
+        <FlexBlock className={styles.btnblock}>
           {!match && (
             <CustomLink className={styles.tag} href="/">
               На главную
             </CustomLink>
           )}
-        </div>
+
+          <Switch onChange={themeContext?.toggleTheme} />
+        </FlexBlock>
 
         <h1 className={styles.title}>GIFS</h1>
         <UserEnterBlock isAuth={isAuth} email={email} />
